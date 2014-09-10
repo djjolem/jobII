6ca793be77e5e32cc358739339e1f8c71d60ce12
Djordje Mijatovic <djjolem@gmail.com>
2014-09-10 23:51:03 +0200

    Add Serbian language

---
+++ app/lang/sr/lcl.php
@@ -0,0 +1,85 @@
+<?php
+
+return array(
+	'productName' => 'JobIT',
+
+	'user' => 'Korisnik',
+
+	'ads' => array(
+		'list' => 'Oglas',
+		'new' => 'Novi oglas',
+		'title' => 'Naslov',
+		'text' => 'Tekst oglasa',
+	),
+
+	'placeholder' => array(
+		'usernameEmail' => 'Korisničko ime ili imejl',
+		'passwordHidden' => '*********',
+		'passwordComment' => '1-8 zankova',
+		'email' => 'imejl',
+		'alias' =>  'Korisničko ime',
+		'additional' => 'Dodatno',
+		'adTitle' => 'Oglas naslov',
+		'shortText' => 'Kratki opis',
+		'adText' => 'Tekst oglasa',
+		'location' => 'Lokacija',
+		'companyName' => 'Ime kompanije',
+	),
+
+	'signup' =>  'Novi nalog',
+	'signin' => 'Prijava',
+	'signout' => 'Odjava',
+	'signinSignup' =>  'Prijava / Novi nalog',
+	'rememberMe' => 'Zapamti me',
+
+	'alias' => 'Alias', 
+	'password' => 'Šifra',
+	'passwordConfirm' =>  'Potvrdi šifru',
+
+	'settings' => 'Podešavanja',
+	'myAds' => 'Moji oglasi',
+	'forgotPass' => 'Šifra zaboravljena',
+	
+	'why?' => 'Zašto?',
+
+	'email' => 'Imejl',
+
+	'or' => 'Ili',
+
+	'submit' => 'Prijavi', // action
+	'close' => 'Zatvori', // action
+	'save' => 'Sačuvaj', //action
+	'clear' => 'Očisti', // action
+
+	'edit..' => 'Uredi..', // action
+	'delete..' => 'Obriši..', // action
+	'apply..' => 'Prijava..', // action
+	'apply' => 'Prijava',
+	'cancel' => 'Odustani',
+	'matching..' => 'Poklapanja..',
+
+	'adDesc' => 'Oglas opis',
+	'fileInput' => 'Fajl',
+	'additional' => 'Dodatno',
+	'aboutCompany' => 'O kompaniji',
+	'shortText' => 'Ukratko',
+
+	'company' => 'Kompanija',
+	'tags' => 'Oznake',
+	'deadline' => 'Deadline',
+
+	'deleteConfirm' => 'Da li želite da obrište reklamu?',
+
+	'longText' => array( 
+		'explainWhy' => 
+				'<p>We need this information so that you can receive access to the site and its content. Rest assured your information will not be sold, traded, or given to anyone.</p>
+         <p></p><br> Please contact us at ' . HTML::link('mailto:suport@jobit.com', 'email') . ' for any other inquiries.</p>',
+   	
+   	'shoulCreateAccount' => 
+   		'<p>Shoul I create an account?</p>
+					<p>Button New account</p>',
+
+		'' => '',
+	), 
+
+);
